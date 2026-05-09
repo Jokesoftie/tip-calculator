8d36c9c80891b6d30819901f9c89e97ee8d2804b
jokesoftie <odujoke25@gmail.com>
2026-05-09 11:56:43 +0100

    submit

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const customEL = document.getElementById("custom");
 const numberOfPeopleEL = document.getElementById("amount-people");
 const tip_amountEL = document.getElementById("tip-amount");
 const total_amountEL = document.getElementById("total-amount");
+const submitBtnEL = document.getElementById("submit");
 const resetBtnEl = document.getElementById("reset");
 
 let bill = 0;
@@ -13,15 +14,12 @@ let totalBill = 0;
 let tipPerPerson = 0;
 let totalAmountPerPerson = 0;
 billEL.addEventListener("input", function () {
-  // console.log(billEL.value);
   bill = Number(billEL.value);
 });
-// console.log(tipsBtnEL);
 tipsBtnEL.forEach((tipBtnEL) => {
   tipBtnEL.addEventListener("click", function () {
     const tip = Number(tipBtnEL.innerHTML.slice(0, -1)) / 100;
     tipAmount = tip * bill;
-    // console.log(tipAmount);
   });
 });
 customEL.addEventListener("input", function () {
@@ -30,29 +28,28 @@ customEL.addEventListener("input", function () {
 });
 numberOfPeopleEL.addEventListener("input", function () {
   numberOfPeople = Number(numberOfPeopleEL.value);
-  
-  // if (!numberOfPeople) {
-  //   console.log("can't be zero");
-  // } else {
-  //   console.log("input number");
-  // }
 });
-document.addEventListener("keydown", function (e) {
-  if (e.key === "Enter") {
-    totalBill = tipAmount + bill;
-    tipPerPerson = tipAmount / numberOfPeople;
-    totalAmountPerPerson = totalBill / numberOfPeople;
+// document.addEventListener("keydown", function (e) {
+//   if (e.key === "Enter") {
+//     totalBill = tipAmount + bill;
+//     tipPerPerson = tipAmount / numberOfPeople;
+//     totalAmountPerPerson = totalBill / numberOfPeople;
 
-    // console.log(tipPerPerson.toFixed(2));
-    // console.log(totalAmountPerPerson.toFixed(2));
-    tip_amountEL.textContent = tipPerPerson.toFixed(2);
-    total_amountEL.textContent = totalAmountPerPerson.toFixed(2);
-  }
+//     tip_amountEL.textContent = tipPerPerson.toFixed(2);
+//     total_amountEL.textContent = totalAmountPerPerson.toFixed(2);
+//   }
+// });
+submitBtnEL.addEventListener("click", function () {
+  totalBill = tipAmount + bill;
+  tipPerPerson = tipAmount / numberOfPeople;
+  totalAmountPerPerson = totalBill / numberOfPeople;
+
+  tip_amountEL.textContent = tipPerPerson.toFixed(2);
+  total_amountEL.textContent = totalAmountPerPerson.toFixed(2);
 });
+
 resetBtnEl.addEventListener("click", function () {
-  // bill=number(billEL.value);
   document.getElementById("bill").value = 0;
-  // document.querySelectorAll(".tip-box").textContent=
   document.getElementById("custom").value = "custom";
   document.getElementById("amount-people").value = 0;
   document.getElementById("tip-amount").textContent = 0.0;
